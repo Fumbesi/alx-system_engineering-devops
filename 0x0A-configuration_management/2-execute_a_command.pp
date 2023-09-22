cc838e84668a85e55e5eb9fadc901b13c992b46e
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-09-22 05:33:43 -0700

    2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #This code will create a manifest that kills a process named killmenow.
 exec { killmenow':
-  command => '/usr/bin/killmenow',
+  command  => '/usr/bin/killmenow',
   provider  => 'shell',
-  return => [0, 1],
+  return  => [0, 1],
 }
